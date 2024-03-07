53374773207c931cdb8dac2d5f683cf3fd9c42b6
Hasa001 <sainiharshit8001@gmail.com>
2024-03-07 18:40:25 +0530

    changed

--- app/api.py
+++ app/api.py
@@ -1,36 +1,36 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import pickle
-import json
+# from fastapi.middleware.cors import CORSMiddleware
+# from pydantic import BaseModel
+# import pickle
+# import json
 
 
 app = FastAPI()
 
-origins = ["*"]
+# origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-class model_input(BaseModel):
+# class model_input(BaseModel):
     
-    Pregnancies : int
-    Glucose : int
-    BloodPressure : int
-    SkinThickness : int
-    Insulin : int
-    BMI : float
-    DiabetesPedigreeFunction :  float
-    Age : int
+#     Pregnancies : int
+#     Glucose : int
+#     BloodPressure : int
+#     SkinThickness : int
+#     Insulin : int
+#     BMI : float
+#     DiabetesPedigreeFunction :  float
+#     Age : int
     
 
-# loading the saved model
-diabetes_model = pickle.load(open('/diabetes_model.sav','rb'))
+# # loading the saved model
+# diabetes_model = pickle.load(open('/diabetes_model.sav','rb'))
 
 
 # @app.post('/diabetes_prediction')
